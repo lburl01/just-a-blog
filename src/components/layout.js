dbeb5c871f7192cc0d3f7af90a0759a3eda5024a
Lori Baumgartner <lori.burleson@gmail.com>
2019-04-17 16:44:19 -0400

    Add helmet page info

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,8 +6,8 @@
  */
 
 import React from "react"
-import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
+import Helmet from 'react-helmet'
 
 import Header from "./header"
 import "./layout.css"
@@ -25,29 +25,28 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
+        <Helmet
+          title={data.site.siteMetadata.title}
+          meta={[
+            { name: 'description', content: 'Sample' },
+            { name: 'keywords', content: 'sample, something' },
+          ]}
+        >
+          <html lang="en" />
+        </Helmet>
         <Header siteTitle={data.site.siteMetadata.title} />
         <div
           style={{
-            margin: `0 auto`,
+            margin: '0 auto',
             maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
+            padding: '0px 1.0875rem 1.45rem',
             paddingTop: 0,
           }}
         >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
+          {children}
         </div>
       </>
     )}
   />
 )
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
-
 export default Layout

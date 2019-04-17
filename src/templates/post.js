801ba340fbef0ef3511231559b38c1c54f9232a0
Lori Baumgartner <lori.burleson@gmail.com>
2019-04-17 16:44:55 -0400

    Add metatags and image

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,14 +1,27 @@
 import React from "react"
 import { graphql } from 'gatsby'
+import Img from 'gatsby-image';
 
 import Layout from '../components/layout'
+import Metatags from '../components/metatags'
 
-const Post = ({ data }) => {
+const Post = ({ data, location }) => {
   const post = data.markdownRemark;
-  const { title } = post.frontmatter;
+  const { description, image, title } = post.frontmatter;
+  const url = '';
+  const thumbnail = image.childImageSharp.resize.src
+
   return (
     <Layout>
+      <Metatags
+        title={title}
+        description={description}
+        thumbnail={url + thumbnail}
+        url={url}
+        pathname={location.pathname}
+      />
       <h2>{title}</h2>
+      <Img fluid={image.childImageSharp.fluid} />
       <div dangerouslySetInnerHTML={{ __html: post.html }} />
     </Layout>
   )
@@ -21,8 +34,19 @@ export const query = graphql`
        frontmatter {
         title
         description
-       }
+        image {
+          childImageSharp {
+            resize(width: 1500, height: 1500) {
+              src
+            }
+            fluid(maxWidth: 786) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
    }
-}`
+}
+`
 
 export default Post;

21cdcc9901c6dfddbe55f77b70b605f593eee320
Lori Baumgartner <lori.burleson@gmail.com>
2019-04-17 15:57:10 -0400

    Create index of posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Hi people</h1>
@@ -15,7 +15,35 @@ const IndexPage = () => (
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
+    {data.allMarkdownRemark.edges.map(({ node }, index) => (
+      <Link to={node.fields.slug} className="link" key={index}>
+        <div className="post-list">
+          <h1>{node.frontmatter.title}</h1>
+          <span>{node.frontmatter.date}</span>
+          <p>{node.excerpt}</p>
+        </div>
+      </Link>
+    ))}
   </Layout>
 )
+
+export const listQuery = graphql`
+  query ListQuery {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          fields{
+            slug
+          }
+          excerpt(pruneLength: 250)
+          frontmatter {
+            date(formatString: "MMMM Do YYYY")
+            title
+          }
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
